6c1ffd86a82c9ddbf33687af2826e7b620175fb6
dvtzoe <ohmtzoe@gmail.com>
2025-11-11 09:29:22 +0700

    w2e3

---
+++ week02/src/ex3.py
@@ -0,0 +1,22 @@
+class Car:
+    def __init__(
+        self,
+        maker: str,
+        model: str,
+        year: str,
+    ):
+        self.maker: str = maker
+        self.model: str = model
+        self.year: str = year
+        self.odometer: int = 0
+
+    def get_d_name(self):
+        return f"{self.year} {self.maker} {self.model}".title()
+
+    def read_odometer(self):
+        return f"This car has {self.odometer} km on it!"
+
+
+my_new_car = Car("Honda", "Civic", "2020")
+my_new_car.odometer = 23
+my_new_car.read_odometer()
